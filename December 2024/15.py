03a5600c1930df75d08a1ac9ad24bbcc7b2c3a4f
prakharmishra2002 <103823400+prakharmishra2002@users.noreply.github.com>
2024-12-16 22:10:09 +0530

    Create 15.py

---
+++ December 2024/15.py
@@ -0,0 +1,12 @@
+class Solution:
+    def peakElement(self, arr):
+        low, high = 0, len(arr) - 1
+
+        while low < high:
+            mid = low + (high - low) // 2
+            if arr[mid] > arr[mid + 1]:
+                high = mid
+            else:
+                low = mid + 1
+        
+        return low  
