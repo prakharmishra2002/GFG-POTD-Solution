0424b066ffd1aece8aa06654d3e984f68a32cf9f
prakharmishra2002 <103823400+prakharmishra2002@users.noreply.github.com>
2024-12-19 23:17:32 +0530

    init

---
+++ December 2024/18.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findPages(self, arr, k):
+        n = len(arr)
+        if k > n:
+            return -1  
+
+        low, high = max(arr), sum(arr)  
+
+        while low < high:
+            mid = (low + high) // 2
+            students, current_sum = 1, 0
+
+            for pages in arr:
+                if current_sum + pages > mid:
+                    students += 1
+                    current_sum = pages
+                else:
+                    current_sum += pages
+
+            if students > k:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
