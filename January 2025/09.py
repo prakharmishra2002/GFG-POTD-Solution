6c5277cf1cb084e8ea9e098ae17a42fd742f23bc
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-01-09 11:01:58 +0530

    Create 09.py

---
+++ January 2025/09.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subarraySum(self, arr, target):
+        current_sum = 0
+        start = 0
+        for end in range(len(arr)):
+            current_sum += arr[end]
+            while current_sum > target and start <= end:
+                current_sum -= arr[start]
+                start += 1
+            if current_sum == target:
+                return [start + 1, end + 1]
+        return [-1]
