4a2229509e1973e1922dac119b84e8a8d1e5a1a4
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-01-24 10:36:26 +0530

    Create 24.cpp

---
+++ January 2025/24.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position) {
+    if (position == 0)
+        return;
+
+    Node* walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
