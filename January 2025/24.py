429efa897d089d350c853dce1f41ed978d095810
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-01-24 10:36:51 +0530

    Create 24.py

---
+++ January 2025/24.py
@@ -0,0 +1,8 @@
+class Solution:
+    def detectLoop(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+            if slow == fast:
+                return True
+        return False
