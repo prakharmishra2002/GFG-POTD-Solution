224b077da8b4b14170549ddd729b67fbbbdb2e44
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-01-26 17:55:59 +0530

    Update 26.cpp

--- January 2025/26.cpp
+++ January 2025/26.cpp
@@ -1,9 +1,11 @@
-class Solution:
-    def removeLoop(self, head):
-        slow, fast = head, head
-        while fast and fast.next and (slow := slow.next) != (fast := fast.next.next): pass
-        if not fast or not fast.next: return
-        slow = head
-        while slow != fast: slow, fast = slow.next, fast.next
-        while fast.next != slow: fast = fast.next
-        fast.next = None
+class Solution {
+public:
+    void removeLoop(Node* head) {
+        Node *slow = head, *fast = head;
+        while (fast && fast->next && (slow = slow->next) != (fast = fast->next->next));
+        if (!fast || !fast->next) return;
+        for (slow = head; slow != fast; slow = slow->next, fast = fast->next);
+        while (fast->next != slow) fast = fast->next;
+        fast->next = nullptr;
+    }
+};
