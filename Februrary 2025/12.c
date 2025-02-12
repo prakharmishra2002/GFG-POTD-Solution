7658c6382ed4e49abf2ac8be6f6f9ebf18a1e103
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-02-12 10:46:50 +0530

    Create 12.c

---
+++ Februrary 2025/12.c
@@ -0,0 +1,20 @@
+int kthSmallest(struct Node* root, int k) {
+    while (root) {
+        if (!root->left) {
+            if (--k == 0) return root->data;
+            root = root->right;
+        } else {
+            struct Node *pre = root->left;
+            while (pre->right && pre->right != root) pre = pre->right;
+            if (!pre->right) {
+                pre->right = root;
+                root = root->left;
+            } else {
+                pre->right = NULL;
+                if (--k == 0) return root->data;
+                root = root->right;
+            }
+        }
+    }
+    return -1;
+}
