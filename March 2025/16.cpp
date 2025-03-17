ef5f955eb9f3a78a63c1721204ae8b64ed0547bd
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-03-17 17:37:16 +0530

    Create 16.cpp

---
+++ March 2025/16.cpp
@@ -0,0 +1,63 @@
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size(), jumps = 0, farthest = 0, end = 0;
+        if (n == 1) return 0;
+        for (int i = 0; i < n - 1; i++) {
+            farthest = max(farthest, i + arr[i]);
+            if (i == end) {
+                jumps++;
+                end = farthest;
+                if (end >= n - 1) return jumps;
+            }
+        }
+        return -1;
+    }
+};
+
+2)
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++)
+                if (j + arr[j] >= i && dp[j] != INT_MAX)
+                    dp[i] = min(dp[i], dp[j] + 1);
+        return dp[n-1] == INT_MAX ? -1 : dp[n-1];
+    }
+};
+
+
+3)
+class Solution {
+  public:
+    int minJumps(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 1) return 0;
+        queue<int> q;
+        vector<bool> visited(n, false);
+        q.push(0);
+        visited[0] = true;
+        int jumps = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            while (size--) {
+                int i = q.front();
+                q.pop();
+                for (int j = 1; j <= arr[i]; j++) {
+                    int next = i + j;
+                    if (next >= n - 1) return jumps + 1;
+                    if (!visited[next]) {
+                        visited[next] = true;
+                        q.push(next);
+                    }
+                }
+            }
+            jumps++;
+        }
+        return -1;
+    }
+};
