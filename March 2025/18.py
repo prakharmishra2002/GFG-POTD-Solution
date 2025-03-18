89b600d03d9b66aff528177e8350caf4b2376f58
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-03-18 17:20:12 +0530

    Create 18.py

---
+++ March 2025/18.py
@@ -0,0 +1,10 @@
+class Solution:
+    def equalPartition(self, arr):
+        s = sum(arr)
+        if s % 2: return False
+        target, dp = s // 2, [False] * (s // 2 + 1)
+        dp[0] = True
+        for num in arr:
+            for j in range(target, num - 1, -1):
+                dp[j] |= dp[j - num]
+        return dp[target]
