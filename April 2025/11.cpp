b61f7ced967b73876923da43ff97b40d58742c0d
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2025-04-11 16:24:04 +0530

    Create 11.cpp

---
+++ April 2025/11.cpp
@@ -0,0 +1,80 @@
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int, int>>> adj(V);
+        for (auto &e : edges)
+            adj[e[0]].push_back({e[1], e[2]});
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, src});
+        while (!pq.empty()) {
+            int d = pq.top().first, u = pq.top().second;
+            pq.pop();
+            if (d > dist[u]) continue;
+            for (auto &p : adj[u]) {
+                int v = p.first, w = p.second;
+                if (dist[u] + w < dist[v]) {
+                    dist[v] = dist[u] + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+        return dist;
+    }
+};
+
+2)
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int, int>>> g(V);
+        for (auto &e : edges) g[e[0]].emplace_back(e[1], e[2]);
+        vector<int> d(V, 1e9); d[src] = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+        q.emplace(0, src);
+        while (!q.empty()) {
+            auto p = q.top(); q.pop();
+            if (p.first > d[p.second]) continue;
+            for (auto &x : g[p.second])
+                if (p.first + x.second < d[x.first])
+                    q.emplace(d[x.first] = p.first + x.second, x.first);
+        }
+        return d;
+    }
+};
+
+
+3)
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int, int>>> g(V);
+        for (auto &e : edges) g[e[0]].emplace_back(e[1], e[2]);
+
+        vector<int> d(V, 1e9);
+        d[src] = 0;
+        set<pair<int, int>> st;
+        st.emplace(0, src);
+
+        while (!st.empty()) {
+            pair<int, int> p = *st.begin();
+            int du = p.first;
+            int u = p.second;
+            st.erase(st.begin());
+
+            for (int i = 0; i < g[u].size(); ++i) {
+                int v = g[u][i].first;
+                int w = g[u][i].second;
+
+                if (du + w < d[v]) {
+                    st.erase({d[v], v});
+                    d[v] = du + w;
+                    st.emplace(d[v], v);
+                }
+            }
+        }
+
+        return d;
+    }
+};
