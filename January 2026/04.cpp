7507d475318b11978ad925c71e11eebec0d4b675
Prakhar Mishra <103823400+prakharmishra2002@users.noreply.github.com>
2026-01-04 11:15:23 +0530

    Create 04.cpp

---
+++ January 2026/04.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void sort012(vector<int>& arr) {
+        int low = 0;
+        int mid = 0;
+        int high = arr.size() - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            }
+            else if (arr[mid] == 1) {
+                mid++;
+            }
+            else {  // arr[mid] == 2
+                swap(arr[high], arr[mid]);
+                high--;
+            }
+        }
+    }
+};
